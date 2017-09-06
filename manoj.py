c25c9d6a6db422920ef502d60dc15022850c3ab2
Balamurali <balamuralim.1993@gmail.com>
2017-09-06 14:26:43 +0530

    Draw contours and shapes
    Using opencv python contours and regular shapes are drawn

---
+++ manoj.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  6 13:40:22 2017
+
+@author: balamurali
+"""
+
+#import cv2
+#import numpy as np
+#from matplotlib import pyplot as py
+#
+#
+#img = np.zeros((300,600,3))
+#img = np.uint8(img)
+#img[0:100,:,0] = 255
+#cv2.imshow('win',img)
+#cv2.waitKey(0)
+
+import numpy as np
+import cv2
+
+yellow = (253,243,0)
+boundary= (0,165,233)
+orange = (255,127,40)
+royalblue = (63,71,204)
+crimson = (238,53,67)
+
+## Create a black image
+img = np.zeros((340,640,3), np.uint8)
+cv2.rectangle(img,(25,25),(610,310),boundary,3)
+cv2.line(img,(280,280),(380,280),royalblue,3)
+cv2.line(img,(280,42),(380,42),yellow,3)
+cv2.line(img,(45,125),(45,205),royalblue,3)
+cv2.line(img,(590,125),(590,205),crimson,3)
+
+contours1 = [np.array([[45,45],[100,45],[45,100],[45,45]])]
+cv2.drawContours(img,contours1,0,yellow,-1)
+
+contours2 = [np.array([[540,45],[595,45],[595,100],[540,45]])]
+cv2.drawContours(img,contours2,0,orange,-1)
+
+contours3 = [np.array([[595,225],[595,280],[540,280],[595,225]])]
+cv2.drawContours(img,contours3,0,orange,-1)
+
+contours4 = [np.array([[45,225],[45,280],[100,280],[45,225]])]
+cv2.drawContours(img,contours4,0,royalblue,-1)
+
+
+img1 = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
+
+#img = cv
+
+
+cv2.imshow('win',img1)
+cv2.waitKey(0)
